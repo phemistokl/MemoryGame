c319b31b95f162b8a24be79c72d5fe7abfadf966
Albert Kumyshev <phemistokl@gmail.com>
2017-03-19 20:15:35 +0300

    dd

--- src/utils/memorygame.js
+++ src/utils/memorygame.js
@@ -2,45 +2,6 @@ import { fromJS, Map, List } from 'immutable';
 
 import repeat from './repeat';
 
-// let _url = [];
-//
-// function generateUrl() {
-//     let images = [];
-//     for (let i = 1; i < 9; i++) {
-//         images.push("images/" + i + ".jpg");
-//     }
-//     images = images.concat(images)
-//     .sort(() => Math.random() - 0.5);
-//     for (let i = 0; i < images.length; i++) {
-//         _url.push({
-//             image: images[i]
-//         });
-//     }
-// }
-//
-// generateUrl();
-//
-// function generateId() {
-//     let currentCount = 0;
-//
-//     function counter() {
-//         return currentCount++;
-//     }
-//
-//     return counter;
-// }
-//
-// let counter = generateId();
-//
-//
-// export function generateBoard({ cols, rows }) {
-//     const cell = Map({ isRevealed: false, isMatched: false });
-//
-//     const AllCells = repeat(16, cell);
-//
-//     return AllCells.map((tile, idx) => tile.set('id', idx).set('img_url', _url[counter()].image));
-// }
-
 let _board =[];
 
 function generateBoard() {
